97d4927608428152cd744478cf95a0ab0df7f1de
RomanLimonta <Rolimonta@gmail.com>
2023-10-10 23:31:42 -0400

    Completed Exercise01_05. Completed Exercise01_06. Completed Exercise01_07.

--- src/ch_01/Exercise01_05.java
+++ src/ch_01/Exercise01_05.java
@@ -2,7 +2,6 @@ package ch_01;
 
 public class Exercise01_05 {
     public static void main(String[] args) {
-        System.out.println("Testing");
-        System.out.println("Good Test");
+        System.out.println( ( (9.5*4.5) - (2.5*3) ) / (45.5-3.5) );
     }
 }
