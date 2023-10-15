e01b3112b48cb5569eb5ac9e66ed30eff019d6d3
RomanLimonta <Rolimonta@gmail.com>
2023-10-14 22:03:47 -0400

    Completed Exercise02_01. Created ch_02 package. Created file for Exercise02_02.

---
+++ src/ch_02/Exercise02_02.java
@@ -0,0 +1,20 @@
+package ch_02;
+
+public class Exercise02_02 {
+    /*
+    (Compute the volume of a cylinder) Write a program that reads in the radius and length of a cylinder and computes
+     the area and volume using the following formulas:
+     area = radius * radius * pi
+     volume = area * length
+     **/
+    /*
+     Here is a sample run:
+     Enter the radius and length of a cylinder: 5.5 12
+     The area is 95.0331
+     The volume is 1140.4
+     **/
+
+    public static void main(String[] args) {
+
+    }
+}
