016c3f9350b71253d2a22dd2574d763f00ce9670
RomanLimonta <Rolimonta@gmail.com>
2023-10-15 20:42:21 -0400

    Completed Exercise02_02.

--- src/ch_02/Exercise02_01.java
+++ src/ch_02/Exercise02_01.java
@@ -7,11 +7,12 @@ public class Exercise02_01 {
     (Convert Celsius to Fahrenheit) Write a program that reads a Celsius degree in a double value from the console,
     then converts it to Fahrenheit and displays the result. The formula for the conversion is as follows:
     Fahrenheit = (9 / 5) * celsius + 32
-*/
+    */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a degree in celsius: ");
         convertCelsiusToFahrenheit(input.nextDouble());
+        input.close();
     }
 
     public static void convertCelsiusToFahrenheit(double celsius){
--- src/ch_02/Exercise02_02.java
+++ src/ch_02/Exercise02_02.java
@@ -1,13 +1,14 @@
 package ch_02;
 
+import java.util.Scanner;
+
 public class Exercise02_02 {
-    /*
+     /*
     (Compute the volume of a cylinder) Write a program that reads in the radius and length of a cylinder and computes
      the area and volume using the following formulas:
      area = radius * radius * pi
      volume = area * length
-     **/
-    /*
+
      Here is a sample run:
      Enter the radius and length of a cylinder: 5.5 12
      The area is 95.0331
@@ -15,6 +16,18 @@ public class Exercise02_02 {
      **/
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius and length of a cylinder separated by a space: ");
+        cylinderAreaAndVolumeCalculator(input.nextDouble(), input.nextDouble());
+        input.close();
 
+    }
+
+    public static void cylinderAreaAndVolumeCalculator(double radius, double length) {
+        double area = radius * radius * Math.PI;
+        double volume = area * length;
+
+        System.out.printf("The area is %.2f: \n", area);
+        System.out.printf("The volume is %.2f: \n", volume);
     }
 }
