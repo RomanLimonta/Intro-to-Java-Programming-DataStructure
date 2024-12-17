3939ec73939cc37f97cf5921f069e67cf072178b
roman <Rolimonta@gmail.com>
2024-12-17 09:24:08 -0500

    Complete Exercise03_23.java. Create file for Exercise03_24.java.

--- src/ch_03/Exercise03_23.java
+++ src/ch_03/Exercise03_23.java
@@ -1,11 +1,14 @@
 package ch_03;
 
+import java.util.Scanner;
+
 public class Exercise03_23 {
     /*
     * (Geometry: point in a rectangle?) Write a program that prompts the user to enter a point (x,y) and checks whether
     * the point is within the rectangle centered at (0,0) with width 10 and height 5. For example (2,2) is inside the
     * rectangle and (6,4) is outside the rectangle, as shown in figure 3.7b (Hint: A point is in the rectangle if its
-    * horizontal distance to (0,0) is less than or equal to 5.0/2. Test your program to cover all cases.
+    * horizontal distance to (0,0) is less than or equal to 10.0/2 and its vertical distance (0,0) is less than or equal
+    * to 5.0/2. Test your program to cover all cases.
     *
     * Here are two samples
     * Enter a point with two coordinates: -4.9 2.49
@@ -16,5 +19,18 @@ public class Exercise03_23 {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("""
+                A Rectangle is centered at (0, 0) with width 10 and height 5.
+                Enter one point (x y) seperated by a space.
+                The program will determine if the point is within the Rectangle: (e.g. 5 8)""");
+        double x = input.nextDouble();
+        double y = input.nextDouble();
+
+        if(x<= 10.0/2 && x>-5 && y<=5.0/2 && y>-2.49){
+            System.out.println("Point (" + x + ", " + y + ") is in the Rectangle");
+        } else {
+            System.out.println("Point (" + x + ", " + y + ") is not in the Rectangle");
     }
+}
 }
---
+++ src/ch_03/Exercise03_24.java
@@ -0,0 +1,16 @@
+package ch_03;
+
+public class Exercise03_24 {
+
+    /*
+    * (Game:pick a card) Write a program that simulates picking a card from a deck of 52 cards. Your program should
+    * display the rank (Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King) and suit (Clubs, Diamonds, Hearts, Spades)
+    * of the card. Here is a sample run of the program:
+    *
+    * The card you picked is Jack of Hearts*/
+
+
+    public static void main(String[] args) {
+
+    }
+}
