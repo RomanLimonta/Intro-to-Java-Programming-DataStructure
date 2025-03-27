a4c3fddeb5f160ab9c44b2385172c632ebf3de7e
roman <Rolimonta@gmail.com>
2025-03-27 12:45:16 -0400

    Create file for Exercise03_28.java. Completed Exercise03_27.

--- src/ch_03/Exercise03_27.java
+++ src/ch_03/Exercise03_27.java
@@ -10,7 +10,6 @@ package ch_03;
  * The point is in the triangle
  * */
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Exercise03_27 {
@@ -22,13 +21,35 @@ public class Exercise03_27 {
         double x = scanner.nextDouble();
         double y = scanner.nextDouble();
 
-       // Area A = [ x1(y2 – y3) + x2(y3 – y1) + x3(y1-y2)]/2
 
-        //(0,0) (200-0) (0-100)
-        //x1 y1  x2 y2   x3 y3
 
-        //double areaOfTr
+        //Given points:  x1 y1   x2  y2    x3 y3
+        //              A(0,0) B(200, 0) C(0, 100)
 
+
+        //Area of triangle = [ x1(y2 – y3) + x2(y3 – y1) + x3(y1-y2)]/2
+        double areaOfTriangle = Math.abs(((0*(0 - 100)) + (200*(100 - 0)) + (0*(0- 0)))) /2.0;
+        //System.out.println("The area of the triangle is " + areaOfTriangle);
+
+        //area of sub triangle using given point.
+        //Triangle PAB (where A = (0, 0), B = (200, 0), and P = (x, y))
+        //Triangle PBC (where B = (200, 0), C = (0, 100), and P = (x, y))
+        //Triangle PCA (where C = (0, 100), A = (0, 0), and P = (x, y))
+        double subTriangleArea1 = Math.abs((0*(0-y) + 200*(y-0) + x*(0-0))) /2.0;
+        //System.out.println("The subTriangle area is " + subTriangleArea1);
+        double subTriangleArea2 = Math.abs(200*(100-y) + 0*(y-0) + x*(0-100)) /2.0;
+        //System.out.println("The subTriangle area is " + subTriangleArea2);
+        double subTriangleArea3 = Math.abs(0*(0 - y) + 0*(y-100) + x*(100-0)) /2.0;
+        //System.out.println("The subTriangle area is " + subTriangleArea3);
+
+        //System.out.println("the sum of the sub triangles is " + (subTriangleArea1 + subTriangleArea2 + subTriangleArea3));
+
+        //compare if sum of sub triangles equals the triangle.
+        if ((subTriangleArea1 + subTriangleArea2 + subTriangleArea3) == areaOfTriangle) {
+            System.out.println("The point (" + x +", " + y  +") is within the triangle.");
+        } else {
+            System.out.println("The point (" + x +", " + y + ") is not within the triangle.");
+        }
     }
 
 }
---
+++ src/ch_03/Exercise03_28.java
@@ -0,0 +1,27 @@
+package ch_03;
+
+/*
+* (Geometry: two rectangles) Write a program that prompts the user to enter the center x-, y- coordinates, width, and
+* height of two rectangles and determine whether the second rectangle is inside the first or overlaps with the first.
+* Test your program to cover all cases.
+*
+* Here are the sample runs:
+*
+* Enter r1's center x-, y-coordinates, width, and height: 2.5 4 2.5 43
+* Enter r2's center x-, y-coordinates, width, and height: 1.5 5 0.5 3
+* r2 is inside r1
+*
+* Enter r1's center x-, y-coordinates, width, and height: 1 2 3 5.5
+* Enter r2's center x-, y-coordinates, width, and height: 3 4 4.5 5
+* r2 overlaps r1
+*
+* Enter r1's center x-, y-coordinates, width, and height: 1 2 3 3
+* Enter r2's center x-, y-coordinates, width, and height: 40 45 3 2
+* r2 is inside r1
+* */
+
+public class Exercise03_28 {
+    public static void main(String[] args) {
+
+    }
+}
