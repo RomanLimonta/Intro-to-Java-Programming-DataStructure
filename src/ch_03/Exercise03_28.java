36560881b7c15dd86d9795bdb1077345fa481310
roman <Rolimonta@gmail.com>
2025-03-28 18:07:12 -0400

    Create file for Exercise03_29.java. Completed Exercise03_28.

--- src/ch_03/Exercise03_28.java
+++ src/ch_03/Exercise03_28.java
@@ -20,8 +20,44 @@ package ch_03;
 * r2 is inside r1
 * */
 
+import java.util.Scanner;
+
 public class Exercise03_28 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println
+                ("Enter r1's center x-, y-coordinates, width, and height: (four numbers separated by space): ");
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        double width1 = scanner.nextDouble();
+        double height1 = scanner.nextDouble();
+        double x1min = x1 - width1/2;
+        double y1min = y1 - height1/2;
+        double x1max = x1 + width1/ 2;
+        double y1max = y1 + height1/2;
+
+        System.out.println
+                ("Enter r2's center x-, y-coordinates, width, and height: (four numbers separated by space): ");
+        double x2 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
+        double width2 = scanner.nextDouble();
+        double height2 = scanner.nextDouble();
+        double x2min = x2 - width2/2;
+        double y2min = y2 - height2/2;
+        double x2max = x2 + width2/2;
+        double y2max = y2 + height2/2;
+
+        scanner.close();
+
+        if (x1min <= x2min && x1max >= x2max && y1min <= y2min && y1max >= y2max){
+            System.out.println("r2 is within r1");
+        } else if (x1max >= x2min && x2max >= x1min && y1max >= y2min && y2max >= y1min){
+            System.out.println("r2 overlaps with r1");
+        } else {
+            System.out.println("r2 does not overlaps with r1");
+        }
 
     }
 }
---
+++ src/ch_03/Exercise03_29.java
@@ -0,0 +1,17 @@
+package ch_03;
+/*
+*(Geometry: two circles) Write a program that prompt the user to enter the center coordinate and radii of two circles
+* and determines whether the second circle is inside the first or overlaps with the first.
+* (Hint: Circle2 is inside Circle1 if the distance between the two centers <= r1 - r2 and overlap Circle1 if the
+* distance between the two center <= r1 + r2. Test your program to cover all cases.)
+* Here are the sample runs:
+* Enter circle1's center x-, y-coordinates, and radius: 0.5 5.1 13
+* Enter circle2's center x-, y-coordinates, and radius: 1 1.7 4.5
+* circle2 is inside circle1
+* */
+
+public class Exercise03_29 {
+    public static void main(String[] args) {
+
+    }
+}
