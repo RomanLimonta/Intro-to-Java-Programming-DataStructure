469be81fba667bcf04a87396382840c7b1183d6a
roman <Rolimonta@gmail.com>
2025-04-02 11:13:17 -0400

    Completed Exercise04_02 Created file Exercise04_03

--- src/ch_04/Exercise04_02.java
+++ src/ch_04/Exercise04_02.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
 *
 * Enter point 1 (latitude and longitude) in degrees: 39.55 -116.25
 * Enter point 2 (latitude and longitude) in degrees: 41.5 87.37
-* The distance between the two points is 1069.79183231593 km
+* The distance between the two points is 10691.79183231593 km
 * */
 public class Exercise04_02 {
 
@@ -26,7 +26,6 @@ public class Exercise04_02 {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter point 1 (latitude and longitude) in degrees: ");
-
         double lat1 = Math.toRadians(scanner.nextDouble());
         double lon1 = Math.toRadians(scanner.nextDouble());
 
@@ -34,7 +33,7 @@ public class Exercise04_02 {
         double lat2 = Math.toRadians(scanner.nextDouble());
         double lon2 = Math.toRadians(scanner.nextDouble());
 
-        double distance = 6371 * Math.acos( Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+        double distance = 6371 * Math.acos( (Math.sin(lat1) * Math.sin(lat2)) + (Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2)));
         System.out.println("The distance between the two points is " + distance + " km");
         scanner.close();
     }
---
+++ src/ch_04/Exercise04_03.java
@@ -0,0 +1,21 @@
+package ch_04;
+
+/*
+* (Geography: estimate areas) Use the GPS locations for Atlanta, Georgia; Orlando, Florida; Savanah, Georgia: and
+* Charlotte, North Carolina in the figure in Section 4.1 to compute the estimated area enclosed by these four cities.
+* (Hint: Use the formula in Programming Exercise 4.2 to compute the distance between two cities. Divide the polygon
+* int two triangles and use the formula in Programming Exercise 2.19 to compute the area of a triangle.)
+*
+* Atlanta = (33.7489954, -84.3879824)
+* Orlando = (28.5383355, -81.3792365)
+* Savannah =(32.0835407, -81.0998342)
+* Charlotte = (35.2270869, -80.8431267)
+*
+* */
+
+public class Exercise04_03 {
+
+    public static void main(String[] args) {
+
+    }
+}
