c76d8c6f8f50430092d17df0925336ca2d045b93
roman <Rolimonta@gmail.com>
2025-11-28 13:08:56 -0500

    Completed Exercise04_04.java Created file Exercise04_05.java

--- src/ch_04/Exercise04_04.java
+++ src/ch_04/Exercise04_04.java
@@ -1,4 +1,28 @@
 package ch_04;
 
+import java.util.Scanner;
+
+/*
+* (Geometry: area of a hexagon) The area of a hexagon can be computed using the following formula
+* (s is the length of a side):
+*
+*                           Area =  6 * s^2 / 4 * tan(pie/6)
+*
+* Write a program that prompts the user  to enter the side of a hexagon and displays its area.
+* Here is a sample run:
+*
+* Enter the side: 5.5
+* The area of the hexagon is 78.59
+* */
 public class Exercise04_04 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the side of a hexagon:");
+
+        double side = scanner.nextDouble();
+        double hexagonArea = (6 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI/6) );
+
+        System.out.printf("The are of the hexagon is %.2f", hexagonArea);
+    }
 }
--- src/ch_04/Exercise04_05.java
+++ src/ch_04/Exercise04_05.java
@@ -1,4 +1,25 @@
 package ch_04;
 
+/*
+* (Geometry: area of a regular polygon) A regular polygon is an n-sided polygon in which all sides are the same length
+* and all angles have the same degree (i.e., the polygon is both equilateral and equiangular). The formula for computing
+* the area of a regular polygon is
+*
+*                   Area n * s^2 / 4 * tan(pi/n)
+*
+* Here s is the length of a side. Write a program that prompts the user to enter the number of sides and their length of
+* a regular polygon and display its area.
+*
+* Here is a sample run:
+*
+* Enter the number of sides: 5
+* Enter the sides length : 6.5
+* The area of the polygon is 72.69017017488385
+* */
+
 public class Exercise04_05 {
+
+    public static void main(String[] args){
+
+    }
 }
